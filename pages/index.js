7b4c37d5ba67bda9a667bfdac8fe0f02a5d4a4d9
Aldi-H <aldihimawan88@gmail.com>
2022-11-18 04:31:24 +0700

    style: add layout n remove navbar

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,12 @@ import { useEffect, useState } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "../components/navbar";
 import styles from "../styles/Home.module.css";
 
 import backend from "../api/backend";
 import { getAllMahasiswa } from "../api/backend";
 import axios from "axios";
+import Layout from "../components/layout";
 
 // export const getStaticProps = async () => {
 //   const res = await backend.get("/mahasiswa");
@@ -53,46 +53,47 @@ export default function Home() {
   }, []);
 
   return (
-    <Box
-      justify="center"
-      align="center"
-      minH="100vh"
-      bg={useColorModeValue("gray.50", "gray.800")}
-      pt={5}
-      pb={10}
-      px={10}
-    >
-      <Navbar />
+    <Layout>
       <Box
-        rounded="lg"
-        bg={useColorModeValue("white", "gray.700")}
-        p={8}
-        boxShadow="lg"
+        justify="center"
+        align="center"
+        minH="100vh"
+        bg={useColorModeValue("gray.50", "gray.800")}
+        pt={5}
+        pb={10}
+        px={10}
       >
-        <TableContainer>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>No</Th>
-                <Th>NIM</Th>
-                <Th>Nama</Th>
-                <Th>Angkatan</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {mahasiswas &&
-                mahasiswas.map((mahasiswa, index) => (
-                  <Tr key={mahasiswa.nim}>
-                    <Td>{index + 1}</Td>
-                    <Td>{mahasiswa.nim}</Td>
-                    <Td>{mahasiswa.nama}</Td>
-                    <Td>{mahasiswa.angkatan}</Td>
-                  </Tr>
-                ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
+        <Box
+          rounded="lg"
+          bg={useColorModeValue("white", "gray.700")}
+          p={8}
+          boxShadow="lg"
+        >
+          <TableContainer>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>No</Th>
+                  <Th>NIM</Th>
+                  <Th>Nama</Th>
+                  <Th>Angkatan</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {mahasiswas &&
+                  mahasiswas.map((mahasiswa, index) => (
+                    <Tr key={mahasiswa.nim}>
+                      <Td>{index + 1}</Td>
+                      <Td>{mahasiswa.nim}</Td>
+                      <Td>{mahasiswa.nama}</Td>
+                      <Td>{mahasiswa.angkatan}</Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 }
